1d9b71e6642d13f8c69955decdcf81fab7b34f61
Karen Xiao <karenxiao@college.harvard.edu>
2014-11-30 23:07:03 -0500

    fix to put radicals in correct order

--- scraper.py
+++ scraper.py
@@ -7,6 +7,7 @@ from pattern.web import URL, DOM, plaintext, strip_between
 from pattern.web import NODE, TEXT, COMMENT, ELEMENT, DOCUMENT
 
 chindict = {}
+radicals = []
 
 url = URL("http://www.mdbg.net/chindict/chindict.php?page=radicals")
 dom = DOM(url.download(cached=True))
@@ -14,6 +15,7 @@ dom = DOM(url.download(cached=True))
 radElements = dom('a.rad')
 
 for rad in radElements:
+    radicals.append(rad.content)
     words = []
     wordUrl = URL("http://www.mdbg.net/chindict/" + rad.attrs["href"])
     wordDom = DOM(wordUrl.download(cached=True))
@@ -25,5 +27,6 @@ for rad in radElements:
     chindict[rad.content] = words
 
 with open('chindict.js', 'w') as outfile:
-  json.dump(chindict, outfile)
+    json.dump(radicals, outfile)
+    json.dump(chindict, outfile)
 
