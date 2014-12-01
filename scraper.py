f9c4c29b2dc755f1cead8ea40a7a31e1ff3dc519
Karen Xiao <karenxiao@college.harvard.edu>
2014-11-30 22:34:06 -0500

    created static library of words

---
+++ scraper.py
@@ -0,0 +1,29 @@
+# scraper for scraping the chinese dictionary off of 
+# http://www.mdbg.net/chindict/chindict.php?page=radicals
+# and stores it into a JSON object
+
+import re, json, io
+from pattern.web import URL, DOM, plaintext, strip_between
+from pattern.web import NODE, TEXT, COMMENT, ELEMENT, DOCUMENT
+
+chindict = {}
+
+url = URL("http://www.mdbg.net/chindict/chindict.php?page=radicals")
+dom = DOM(url.download(cached=True))
+
+radElements = dom('a.rad')
+
+for rad in radElements:
+    words = []
+    wordUrl = URL("http://www.mdbg.net/chindict/" + rad.attrs["href"])
+    wordDom = DOM(wordUrl.download(cached=True))
+    wordTable = wordDom.by_class("results")
+
+    for word in wordTable[0].by_tag("span"):
+        words.append(word.content)
+
+    chindict[rad.content] = words
+
+with open('chindict.js', 'w') as outfile:
+  json.dump(chindict, outfile)
+
